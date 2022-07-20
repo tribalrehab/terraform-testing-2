f43d37afc0ee8557a36e65e0922a407dd4a57c27
tribalrehab <tribalrehab@gmail.com>
2022-07-20 16:37:58 -0400

    Update my_terraform_code.tf

--- my_terraform_code.tf
+++ my_terraform_code.tf
@@ -1,3 +1,12 @@
+variable aws_access_key_id{}
+variable aws_secret_access_key{}
+
+provider "aws" {
+	access_key = var.aws_access_key_id
+	secret_key = var.aws_secret_access_key
+	region = "us-east-1"
+}
+
 resource "aws_instance" "my_ec2_instance" {
 	ami = "ami-05912b6333beaa478"
 	instance_type = "t2.micro"
